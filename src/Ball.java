c7abc930aa1348568882b6dff40119fa432ac899
FlorianGelb <Florian.Braun02@gmail.com>
2018-12-25 17:50:49 +0100

    Further clean code

--- src/Ball.java
+++ src/Ball.java
@@ -19,15 +19,8 @@ public class Ball extends Circle implements HarterKoerper {
 	double m = 1;
 	Color c = Color.BLUE;
 
-	/**
-	 * Konstruktor der Ballklasse.
-	 *
-	 * @param bx     x-Koordinate des Startpunktes.
-	 * @param by     y-Koordinate des Startpunktes.
-	 * @param radius Radius des Balls.
-	 * @param col    Farbe des Balls.
-	 */
-	public Ball(double bx, double by, double radius, Color col) {
+	public Ball(double bx, double by, double radius, Color col)
+	{
 		super(bx, by, radius);
 		x = bx;
 		y = by;
@@ -37,34 +30,20 @@ public class Ball extends Circle implements HarterKoerper {
 		ballAnzahl++;
 
 	}
-
-	/**
-	 * Setzt die Farbe des Balls.
-	 *
-	 * @param farbe
-	 */
-	public void setColor(Color farbe) {
+	public void setColor(Color farbe)
+	{
 		c = farbe;
 		setFill(farbe);
 	}
-
-	/**
-	 * Aktualisiert die Ballposition entsprechend der Geschwindigkeitsattribute.
-	 */
-	public void updatePosition() {
-
+	public void updatePosition()
+	{
 		x = x + vx;
 		y = y + vy;
 
 		relocate(x, y);
 	}
-
-	/**
-	 * Diese Methode wird vom Simulationstimer immer wieder aufgerufen.
-	 *
-	 *
-	 */
-	public boolean collideWith(HarterKoerper h) {
+	public boolean collideWith(HarterKoerper h)
+	{
 		if (h.getClass() == Ball.class) {
 			Ball b = (Ball) h;
 			if (((this.x - b.x) * (this.x - b.x) + (this.y - b.y) * (this.y - b.y)) < (this.radius + b.radius) * (this.radius + b.radius)) {
@@ -74,5 +53,4 @@ public class Ball extends Circle implements HarterKoerper {
 		}
 		return false;
 	}
-
 }
--- src/Block.java
+++ src/Block.java
@@ -12,12 +12,6 @@ public class Block extends Rectangle implements HarterKoerper
 
     private Color c = Color.RED;
 
-    private double cx1 = 0;
-    private double cx2 = 0;
-
-    private double cy1 = 0;
-    private double cy2 = 0;
-
     public Block(float x, float y, float width, float hight, Color c)
     {
         super(x,y, width, hight);
@@ -30,17 +24,13 @@ public class Block extends Rectangle implements HarterKoerper
         this.width = width;
 
         this.c = c;
-
-
-
     }
 
     @Override
-    public boolean collideWith(HarterKoerper h) {
-
+    public boolean collideWith(HarterKoerper h)
+    {
         if (h.getClass() == Ball.class)
         {
-
             Ball b = (Ball) h;
 
             if (((b.x >= this.x) && (b.x <= (this.x + this.width))) && ((this.y - b.y) <= b.radius))
@@ -54,17 +44,14 @@ public class Block extends Rectangle implements HarterKoerper
                 System.out.println("2");
                 b.vy = -b.vy;
                 b.vx = -b.vx;
-
             }
             if ((((b.y + b.radius > this.y) && (b.y + b.radius  < (this.y + this.hight))) && b.x + b.radius < this.x + this.width) &&  ((this.x - b.x) <= b.radius))
             {
                 System.out.println("3");
                 b.vy = -b.vy;
                 b.vx = -b.vx;
-
             }
             }
         return false;
     }
-
 }
