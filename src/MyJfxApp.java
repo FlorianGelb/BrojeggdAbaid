1728d1fc66f930fddca0d8d7aca02eb3aa8c48c7
FlorianGelb <Florian.Braun02@gmail.com>
2018-12-25 17:48:12 +0100

    Further clean code

--- src/MyJfxApp.java
+++ src/MyJfxApp.java
@@ -22,7 +22,8 @@ import java.util.Random;
  *   Realisierung in Java
  * 
  */
-public class MyJfxApp extends Application {
+public class MyJfxApp extends Application
+{
 	Group root = new Group();
 	int Anzahl = 500;
 	SimulationTimer simulationLoop;
@@ -33,32 +34,25 @@ public class MyJfxApp extends Application {
 	final BorderPane borderPane = new BorderPane();
 	Scene scene = new Scene(root, 600, 400);
 
-
-	public void start(Stage stage) {
-
-
-		for (int i = 0; i < b.length; i++) {
+	public void start(Stage stage)
+	{
+		for (int i = 0; i < b.length; i++)
+		{
 			b[i] = new Ball(zufall.nextInt(1000), zufall.nextInt(1000), rad, Color.RED);
 
 			if (b[i].x >= scene.getWidth() || b[i].x <= 0)
 			{
 				b[i].x = scene.getHeight() / 2;
 			}
-
 			if (b[i].y >= scene.getHeight() || b[i].y <= 0)
 			{
 				b[i].y = scene.getHeight() / 2;
 			}
-
 			b[i].vx = zufall.nextDouble() * zufall.nextInt(10);
 			b[i].vy = zufall.nextDouble() * zufall.nextInt(10);
 
 			root.getChildren().add(b[i]);
 		}
-		for (int i = 0; i < blocke.length; i++)
-
-
-
 		stage.setTitle("My JavaFX Application");
 		stage.setScene(scene);
 		stage.show();
@@ -66,24 +60,21 @@ public class MyJfxApp extends Application {
 		startSimulation();
 	}
 
-	private void startSimulation() {
+	private void startSimulation()
+	{
 		simulationLoop = new SimulationTimer(this, 1);
 		simulationLoop.start();
 	}
 
-
-	/*
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		launch(args);
 	}
 
-
-	public void updateSimulation() {
-		//updatePosition(myJfxApp);
-		for (int i = 0; i < b.length; i++) {
-
+	public void updateSimulation()
+	{
+		for (int i = 0; i < b.length; i++)
+		{
 			if ((scene.getWidth() - b[i].x <= b[i].radius && b[i].vx > 0) || (b[i].x <= b[i].radius && b[i].vx < 0 ))
 			{
 				b[i].vx = -b[i].vx;
@@ -96,7 +87,6 @@ public class MyJfxApp extends Application {
 
 			for (int n = i + 1; n < b.length; n++)
 			{
-
 				if (b[n].collideWith(b[i]))
 				{
 					double d1 = b[n].vx;
@@ -109,18 +99,9 @@ public class MyJfxApp extends Application {
 					b[i].vy = d2;
 					b[n].updatePosition();
 					b[i].updatePosition();
-
 				}
-
-				//b[n].updatePosition();
-
 			}
 			b[i].updatePosition();
-
-
 		}
-
-
 	}
-
 }
